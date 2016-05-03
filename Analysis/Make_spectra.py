0ecba340e24816c772fe5ae937e2b0b40fa86ee8
Thomas Klijnsma <klthomas@student.ethz.ch>
2016-05-03 16:35:02 +0200

    Modules to analyze output of flashgg

---
+++ Analysis/Make_spectra.py
@@ -0,0 +1,242 @@
+
+from Spectrum import Spectrum
+from Spectrum import Spectra_container
+
+
+import ROOT
+import argparse
+import os
+
+from array import array
+
+
+########################################
+# Main
+########################################
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument( '--redraw', action='store_true', help='Forces the program to redraw the histograms from the root files')
+    args = parser.parse_args()
+
+
+    # ======================================
+    # Set up ROOT
+
+    print 'Importing root libraries'
+    ROOT.gSystem.Load("libDataFormatsFWLite.so")
+    ROOT.AutoLibraryLoader.enable()
+    #ROOT.FWLiteEnabler.enable()
+
+    ROOT.gROOT.SetBatch(True)
+    ROOT.gROOT.ProcessLine("gErrorIgnoreLevel = kError;")
+    #ROOT.gStyle.SetOptFit(1011)
+
+
+    # ======================================
+    # Set up spectrum class
+
+    # kg1_root_file = '../Run_gridpacks_and_shower/Saved_root_files/kg1_gg_H/Showered.root'
+    # kt1_root_file = '../Run_gridpacks_and_shower/Saved_root_files/kt1_gg_H_quark-mass-effects/Showered.root'
+    # analysis_level = 'SHOWERED'
+
+    kg1_root_file = '../Apply_flashgg/Saved_root_files/flashgg_kg1_gg_H.root'
+    kt1_root_file = '../Apply_flashgg/Saved_root_files/flashgg_kt1_gg_H_quark-mass-effects.root'
+    analysis_level = 'CUTS'
+
+    # Initialize
+    spec = Spectra_container( analysis_level )
+
+    spec.Set_data_spectrum(
+        bins = [ 0, 15, 26, 43, 72, 125, 200 ],
+        values   = [ 9.0, 2.0, 3.4, 6.2, 4.6, 2.6, 0.7 ],
+        err_up   = [ 6.4, 4.9, 4.8, 3.7, 2.4, 1.0, 0.5 ],
+        err_down = [ -6.2, -5.5, -4.6, -3.5, -2.7, -1.0, -0.4 ],
+        max_pt = 300.0,
+        )
+
+    if not args.redraw and os.path.isfile('H_kt1.pickle') and os.path.isfile('H_kg1.pickle'):
+        # Load pickle files with histograms in there
+        print 'Reading histograms from .pickle files'
+        spec.kg1.Set_values_from_pickle_file( 'H_kg1.pickle' )
+        spec.kt1.Set_values_from_pickle_file( 'H_kt1.pickle' )
+    else:
+        # Load both MC spectra from their root files (.Draw() command)
+        spec.kg1.Set_values_from_root_file( kg1_root_file, Verbose=True )
+        spec.kt1.Set_values_from_root_file( kt1_root_file, Verbose=True )
+
+    spec.kg1.Print_H()
+    spec.kt1.Print_H()
+
+    Draw_both_unnormalized_spectra( spec )
+    Draw_both_normalized_spectra( spec )
+
+
+
+########################################
+# Functions
+########################################
+
+def Draw_both_unnormalized_spectra( spec ):
+
+    ROOT.gStyle.SetOptStat(0)
+
+    spec.c1.Clear()
+
+    # Convenience pointers
+    kt1 = spec.kt1.H
+    kg1 = spec.kg1.H
+
+    kt1.Draw('LE1')
+    kg1.Draw('LE1HISTSAME')
+
+    kt1.SetTitle('Both p_{t} spectra (unnormalized)')
+    kt1.GetXaxis().SetTitle( 'p_{t} [GeV]' )
+    kt1.GetYaxis().SetTitle( 'Entries' )
+    kt1.GetYaxis().SetTitleOffset(1.30)
+
+
+    # Histogram drawing settings
+    kt1.SetLineWidth(2)
+    kg1.SetLineWidth(2)
+    kg1.SetLineColor(2)
+
+    # Set y-axis limit
+    y_max = 1.1 * max( kt1.GetMaximum(), kg1.GetMaximum() )
+    kt1.SetMaximum( y_max )
+
+    # ======================================
+    # Some information text boxes
+
+    tl = ROOT.TLatex()
+    tl.SetNDC()
+    tl.SetTextSize(0.035)
+    
+    # Convenience variables: nl = next line, nc = next column
+    nl = 0.05
+    nc = 0.15
+    lx = 0.64
+    ly = 0.81
+
+    tl.SetTextColor(1)
+    tl.DrawLatex( lx, ly+nl, 'Total entries:' )    
+    tl.SetTextColor(4)
+    tl.DrawLatex( lx, ly, '#kappa_{t}=1, #kappa_{g}=0:' )
+    tl.DrawLatex( lx+nc, ly, str(int(kt1.GetEntries())) )
+    tl.SetTextColor(2)
+    tl.DrawLatex( lx, ly-nl, '#kappa_{t}=0, #kappa_{g}=1:' )
+    tl.DrawLatex( lx+nc, ly-nl, str(int(kg1.GetEntries())) )
+
+
+    spec.c1.Print( 'Both_unnormalized_spectra.pdf', '.pdf' )
+
+    ROOT.gStyle.SetOptStat(1011)
+
+
+def Draw_both_normalized_spectra( spec ):
+
+    ROOT.gStyle.SetOptStat(0)
+
+    spec.c1.Clear()
+
+    # Convenience pointers
+    kt1 = spec.kt1.H
+    kg1 = spec.kg1.H
+
+    # Normalizes
+    kt1.Scale( spec.kt1.normalization )
+    kg1.Scale( spec.kg1.normalization )
+
+    # # Make data histogram
+    # data = ROOT.TH1F( 'data', 'data',
+    #     spec.n_pt_bins,
+    #     array( 'd', spec.pt_bins ),
+    #     )
+
+    # for i_val in range(spec.n_pt_bins):
+    #     data.SetBinContent( i_val+1, spec.data.values[i_val] )
+
+    # Purely visual minimal data shift
+    data_shift = 2.0
+
+    if not spec.data.err_up == []:
+        data = ROOT.TGraphAsymmErrors(
+            spec.n_pt_bins,
+            array( 'd', [ kt1.GetBinCenter(i+1)+data_shift for i in range(spec.n_pt_bins) ] ),
+            array( 'd', [ i for i in spec.data.values ] ),
+            array( 'd', [ 0 for i in range(spec.n_pt_bins) ] ),
+            array( 'd', [ 0 for i in range(spec.n_pt_bins) ] ),
+            array( 'd', [ -i for i in spec.data.err_down ] ),
+            array( 'd', spec.data.err_up ),
+            )
+    else:
+        data = ROOT.TGraphAsymmErrors(
+            spec.n_pt_bins,
+            array( 'd', [ kt1.GetBinCenter(i+1) for i in range(spec.n_pt_bins) ] ),
+            array( 'd', spec.data.values ),
+            array( 'd', [ 0 for i in range(spec.n_pt_bins) ] ),
+            array( 'd', [ 0 for i in range(spec.n_pt_bins) ] ),
+            array( 'd', [ 0 for i in range(spec.n_pt_bins) ] ),
+            array( 'd', [ 0 for i in range(spec.n_pt_bins) ] ),
+            )
+
+    kt1.Draw("LE1")
+    kg1.Draw('LE1HISTSAME')
+    data.Draw('PSAME')
+
+    kt1.SetTitle('Both p_{t} spectra (normalized)')
+    kt1.GetXaxis().SetTitle( 'p_{t} [GeV]' )
+    kt1.GetYaxis().SetTitle( '#Delta#sigma [fb]' )
+    kt1.GetYaxis().SetTitleOffset(1.30)
+
+    # Histogram drawing settings
+    kt1.SetLineWidth(2)
+    kg1.SetLineWidth(2)
+    kg1.SetLineColor(2)
+    data.SetMarkerStyle(8)
+    #data.SetLineWidth(2)
+    data.SetMarkerColor(1)
+
+    # Set y-axis limit
+    y_max = 1.5 * max( kt1.GetMaximum(), kg1.GetMaximum() )
+    kt1.SetMaximum( y_max )
+
+    # ======================================
+    # Some information text boxes
+
+    tl = ROOT.TLatex()
+    tl.SetNDC()
+    tl.SetTextSize(0.035)
+    
+    # Convenience variables: nl = next line, nc = next column
+    nl = 0.05
+    nc = 0.15
+    lx = 0.64
+    ly = 0.81
+
+    tl.SetTextColor(1)
+    tl.DrawLatex( lx, ly+nl, 'Total cross section:' )
+    tl.SetTextColor(4)
+    tl.DrawLatex( lx, ly, '#kappa_{t}=1, #kappa_{g}=0:' )
+    tl.DrawLatex( lx+nc, ly, '{0:.2f} fb'.format(kt1.Integral()) )
+    tl.SetTextColor(2)
+    tl.DrawLatex( lx, ly-nl, '#kappa_{t}=0, #kappa_{g}=1:' )
+    tl.DrawLatex( lx+nc, ly-nl, '{0:.2f} fb'.format(kg1.Integral()) )
+    tl.SetTextColor(1)
+    tl.DrawLatex( lx, ly-2*nl, 'Data:' )
+    tl.DrawLatex( lx+nc, ly-2*nl, '{0:.2f} fb'.format(sum(spec.data.values)) )
+
+
+    spec.c1.Print( 'Both_normalized_spectra.pdf', '.pdf' )
+
+    ROOT.gStyle.SetOptStat(1011)
+
+
+
+
+########################################
+# End of Main
+########################################
+if __name__ == "__main__":
+    main()
---
+++ Analysis/Spectrum.py
@@ -0,0 +1,239 @@
+#!/usr/bin/env python
+"""
+Thomas:
+"""
+
+########################################
+# Imports
+########################################
+
+
+import os
+import math
+import pickle
+from array import array
+from glob import glob
+from copy import deepcopy
+
+
+########################################
+# ROOT
+########################################
+
+import ROOT
+
+# ======================================
+# Creates arbitrary histogram name by increasing the counter
+rootc = 100
+def Get_Hname():
+    global rootc
+    Hname = 'H' + str(rootc)
+    rootc += 1
+    return Hname
+
+
+########################################
+# Main class
+########################################
+    
+class Spectra_container:
+
+    # ======================================
+    # Initialization
+
+    def __init__( self, analysis_level = 'SHOWERED' ):
+        'Initialization'
+
+        # Possibilities are currently 'SHOWERED' and 'CUTS'
+        # These root files are structured differently, so they have to be read out differently
+        self.analysis_level = analysis_level
+
+
+        # Total fiducial cross section [fb] (from http://arxiv.org/pdf/1508.07819v2.pdf)
+        self.total_XS = 32.2
+
+        # Open up a Spectrum() instances for data, kt1, and kg1
+        self.data = Spectrum( self, 'data' )
+        self.kt1 = Spectrum( self, 'kt1' )
+        self.kg1 = Spectrum( self, 'kg1' )
+
+        self.c1 = ROOT.TCanvas("c1","c1",1000,800)
+        self.c1.SetGrid()
+
+
+    # ======================================
+    # Access functions
+
+    def Set_data_spectrum( self, bins, values, err_up, err_down, max_pt = None ):
+
+        # Determine max_pt; if not specified, round up to nearest fifty
+        if not max_pt:
+            round_to_nearest = 50.0
+            last_bin_pt = bins[-1] + 1
+            max_pt = int(math.ceil(last_bin_pt/round_to_nearest)) * int(round_to_nearest)
+
+        # These are the general variables used to set the bins of all spectra
+        self.n_pt_bins = len(bins)
+        self.pt_bins = bins + [ max_pt ]
+
+        # Load into Spectrum() instance
+        self.data.Set_pt_bins( self.pt_bins )
+        self.data.Set_values_from_list( values, err_up, err_down )
+
+        # Also set pt_bins for the other Spectrum() instances
+        self.kt1.Set_pt_bins( self.pt_bins )
+        self.kg1.Set_pt_bins( self.pt_bins )
+
+
+
+
+class Spectrum:
+
+    # ======================================
+    # Initialization
+
+    def __init__( self, container, name ):
+        
+        # Pointer to container class
+        self.container = container
+        
+        # Name of the spectrum
+        self.name = name
+
+
+    def Set_pt_bins( self, pt_bins ):
+        self.n_pt_bins = len(pt_bins) - 1
+        self.pt_bins = pt_bins
+
+
+    def Set_values_from_list( self, values, err_up = [], err_down = [] ):
+        self.values = values
+        self.err_up = err_up
+        self.err_down = err_down
+
+
+    def Set_values_from_root_file( self, root_file, Verbose = False ):
+        self.root_file = root_file
+
+        if not os.path.isfile( root_file ):
+            print 'Error: root_file {0} does not exist'.format(root_file)
+            return
+
+        # Unique histogram name
+        Hname = Get_Hname()
+        
+        # Title
+        Htitle = self.name
+
+        # Open the tree
+        #   Spectra can be made for showered root files (no cuts applied)
+        #   or cut root files (through the flashgg framework)
+
+        # Open file pointer
+        root_fp = ROOT.TFile( root_file )
+
+        if self.container.analysis_level == 'SHOWERED':
+
+            # Get Events tree
+            tree = root_fp.Get('Events')
+            tree.SetAlias( "gp", "Events.recoGenParticles_genParticles__GEN.obj" )
+
+            #draw_str = 'gp.pt_>>{0}'.format( Hname )
+            #sel_str  = 'gp.status_==22&&gp.pdgId_==25'
+
+            # Create the histogram
+            H = ROOT.TH1F( Hname, Htitle, self.n_pt_bins, array( 'd', self.pt_bins ) )
+
+            draw_str = 'gp.pt()>>{0}'.format( Hname )
+            sel_str  = 'gp.status()==22&&gp.pdgId()==25'
+
+            if Verbose:
+                print 'Applying draw and selection strings for {0}:'.format(self.name)
+                print '  tree.Draw("{0}", "{1}")'.format( draw_str, sel_str )
+            tree.Draw(draw_str, sel_str)
+
+
+        elif self.container.analysis_level == 'CUTS':
+
+            # Enter the directory and create the histogram there
+            #root_fp.ls()
+            root_fp.cd('genDiphotonDumper/trees')
+
+            H = ROOT.TH1F( Hname, Htitle, self.n_pt_bins, array( 'd', self.pt_bins ) )
+
+            tree  = ROOT.gDirectory.Get( 'ggH_all' )
+
+            tree.Draw( 'pt>>' + Hname )
+            
+
+
+        if H.GetEntries() == 0.0:
+            print '\nError: Retrieved histogram is empty'
+            #return
+
+        # Add the overflow bin to the last bin
+        overflow = H.GetBinContent( self.n_pt_bins + 1 )
+        H.SetBinContent( self.n_pt_bins,
+                              H.GetBinContent( self.n_pt_bins ) + overflow )
+        if Verbose:
+            print 'Adding {0} entries from overflow to the last bin (bin {1})'.format(
+                overflow, H.GetNbinsX() )
+
+        # Set unnormalized values by reading bin contents from the histogram
+        self.unnormalized_values = \
+            [ H.GetBinContent(i+1) for i in range(self.n_pt_bins) ]
+
+        # Normlize w.r.t. total fiducial cross section
+        self.normalization = self.container.total_XS / sum(self.unnormalized_values)
+        self.values = [ self.normalization * i for i in self.unnormalized_values ]
+
+        # Deepcopy H into self.H to make sure histogram persists
+        self.H = deepcopy( H )
+
+        # Read histogram into arrays
+        self.Set_values_from_histogram()
+
+        # Save histogram to .pickle to avoid constant redrawing
+        with open( 'H_' + self.name + '.pickle', 'wb' ) as pickle_fp:
+            pickle.dump( H, pickle_fp )
+
+
+    def Set_values_from_pickle_file( self, pickle_file, Verbose = False ):
+
+        with open( pickle_file, 'rb' ) as pickle_fp:
+            self.H = pickle.load( pickle_fp )
+
+        # Read histogram into arrays
+        self.Set_values_from_histogram()
+
+
+    def Set_values_from_histogram( self ):
+        H = self.H
+
+        # Set unnormalized values by reading bin contents from the histogram
+        self.unnormalized_values = \
+            [ H.GetBinContent(i+1) for i in range(self.n_pt_bins) ]
+
+        # Normlize w.r.t. total fiducial cross section
+        self.normalization = self.container.total_XS / sum(self.unnormalized_values)
+        self.values = [ self.normalization * i for i in self.unnormalized_values ]
+
+
+    def Print_H( self ):
+
+        self.container.c1.Clear()
+        self.H.Draw()
+        self.container.c1.Print( self.H.GetTitle() + '.pdf', 'pdf' )
+
+
+
+
+
+
+
+
+########################################
+# Functions
+########################################
+
+
