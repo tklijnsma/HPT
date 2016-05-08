3079d16576a33a1abd53a160179d03bfea2b7e1e
Thomas Klijnsma <klthomas@student.ethz.ch>
2016-05-08 13:51:20 +0200

    Displays what to do if no input was passed

--- Run_gridpacks_and_shower/Save_root_files.sh
+++ Run_gridpacks_and_shower/Save_root_files.sh
@@ -1,13 +1,14 @@
-if [ -z "$1" ]; then
-    return 0
-elif [ ! -d "$1" ]; then
-    return 0
-else
-    RUNNAME="$1"
+if [ $# -eq 0 ]; then
+    echo "    No arguments supplied"
+    echo "    Supply the name of one of the following directories:"
+    ls
+    return 1
 fi
+
+RUNNAME="$1"
 
 DATESTAMP="$(date +'%m%d')"
 
-mkdir -p Saved_root_files/$DATESTAMP_$RUNNAME
-#cp $RUNNAME/*.root Saved_root_files/$DATESTAMP_$RUNNAME/
-mv $RUNNAME/*.root Saved_root_files/$DATESTAMP_$RUNNAME/
+mkdir -p Saved_root_files/${DATESTAMP}_${RUNNAME}
+#cp $RUNNAME/*.root Saved_root_files/${DATESTAMP}_${RUNNAME}/
+mv $RUNNAME/*.root Saved_root_files/${DATESTAMP}_${RUNNAME}/
