c2f76f2040f338270c0aaca69d1f748d1ded395a
Thomas Klijnsma <klthomas@student.ethz.ch>
2016-09-23 14:05:43 +0200

    Updated template to get the extra jet as well

--- Apply_flashgg/Scripts/cfg_template.py
+++ Apply_flashgg/Scripts/cfg_template.py
@@ -17,6 +17,12 @@ process.load("flashgg.MicroAOD.flashggGenPhotons_cfi")
 process.load("flashgg.MicroAOD.flashggGenPhotonsExtra_cfi")
 process.load("flashgg.MicroAOD.flashggGenDiPhotons_cfi")
 
+process.load("PhysicsTools.PatAlgos.slimming.slimmedGenJets_cfi")
+process.load("RecoJets.Configuration.RecoGenJets_cff")
+
+process.load("RecoJets.Configuration.GenJetParticles_cff")
+process.load("RecoJets.JetProducers.fixedGridRhoProducer_cfi")
+
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32( 1000 )
 
@@ -100,6 +106,29 @@ process.myGenDiPhotons = cms.EDFilter("GenDiPhotonSelector",
     )
 
 
+# process.filteredGenJetsEta2p5 = cms.EDFilter(
+#     "GenJetSelector",
+#     src=cms.InputTag("genJetCollection"),
+#     cut=cms.string("pt>%f && abs(eta)<2.5" % jetPtCut),
+#     )
+process.filteredGenJetsEta4p7 = cms.EDFilter(
+    "GenJetSelector",
+    src=cms.InputTag("slimmedGenJets"),
+    cut=cms.string("pt>30 && abs(eta)<4.7"),
+    )
+# process.flashggGenHadronicActivity2p5 = cms.EDProducer(
+#     "FlashggGenHadronicActivityProducer",
+#     src=cms.InputTag("filteredGenJetsEta2p5"),
+#     veto=cms.InputTag(genDiphotons)
+#     )
+process.flashggGenHadronicActivity4p7 = cms.EDProducer(
+    "FlashggGenHadronicActivityProducer",
+    src=cms.InputTag("filteredGenJetsEta4p7"),
+    veto=cms.InputTag("myGenDiPhotons")
+    )
+
+
+
 import flashgg.Taggers.dumperConfigTools as cfgTools
 from flashgg.Taggers.genDiphotonDumper_cfi import genDiphotonDumper
 process.genDiphotonDumper = genDiphotonDumper.clone(
@@ -108,7 +137,29 @@ process.genDiphotonDumper = genDiphotonDumper.clone(
     processId = cms.string("ggH"),
     nameTemplate = cms.untracked.string("$PROCESS_$LABEL_$SUBCAT"),
     dumpTrees = cms.untracked.bool(True)
-)
+    # outputFileName = cms.untracked.string("ditiseentest.root")
+    )
+
+from flashgg.Taggers.globalVariables_cff import globalVariables
+process.genDiphotonDumper.dumpGlobalVariables = True
+process.genDiphotonDumper.globalVariables = cms.PSet(
+    extraFloats=cms.PSet()
+    # outputFileName = cms.untracked.string("ditiseentest.root")
+    )
+
+
+cfgTools.addGlobalFloats(
+    process,
+    # dumper.globalVariables, # dumper was not defined
+    process.genDiphotonDumper.globalVariables,
+    "flashggGenHadronicActivity4p7",
+    [
+        "nJets4p7 := numberOfDaughters",
+         "jet0Pt :=       ? numberOfDaughters > 0 ? daughter(0).pt : 0",
+         "jet0Rapidity := ? numberOfDaughters > 0 ? daughter(0).rapidity : 0",
+        ]
+    )
+
 
 cfgTools.addCategories(process.genDiphotonDumper,
     [ ("all","1",0),  # category definition: (<name>,<selection>,<num_subcats>)
